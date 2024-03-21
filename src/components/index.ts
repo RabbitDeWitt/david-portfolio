43b8f4f505e3259cf5f95934452d788471e9e562
David Coelho <96807782+RabbitDeWitt@users.noreply.github.com>
2024-03-21 14:44:15 -0300

    feat: initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar/Navbar";
+import Hero from "./Hero/Hero";
+import Footer from "./Footer/Footer";
+
+import ProjectCard from "./ProjectCard/ProjectCard";
+import Button from "./Button/Button";
+
+export {
+  Navbar,
+  Hero,
+  Footer,
+  ProjectCard,
+  Button
+}
