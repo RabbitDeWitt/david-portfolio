cd4b7835e7743cfe5a34fe7971ef13051a7a5f90
David Coelho <96807782+RabbitDeWitt@users.noreply.github.com>
2024-03-23 15:33:19 -0300

    fix: skills alignment

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,18 +5,18 @@ const Skills = () => {
   return (
     <section className="bg-tertiary py-12">
       <div className="container mx-auto">
-        <div className="grid grid-cols-2  sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-y-3 ">
+        <div className="flex flex-wrap justify-center gap-y-5">
           {tecnologies.map(({ icon, name }, i) => (
             <div
               key={i}
-              className="flex justify-center items-center"
+              className="flex justify-center items-center lg:w-[14.28%] md:w-[20%] sm:w-[25%] w-[50%]"
             >
               <Image
                 src={icon}
                 width={75}
                 height={75}
                 alt={name}
-                className="object-contain"
+                className="object-contain saturate-0 hover:saturate-100 transition-all duration-300"
               />
             </div>
           ))}
